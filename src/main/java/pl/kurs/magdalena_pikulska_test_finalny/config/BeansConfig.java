05f26b2dd17c07225fbac4226327ad6bfcaf1ae1
Magdalena Pikulska <m.e.pikulska@gmail.com>
2024-07-09 00:53:40 +0200

    commit

--- src/main/java/pl/kurs/magdalena_pikulska_test_finalny/config/BeansConfig.java
+++ src/main/java/pl/kurs/magdalena_pikulska_test_finalny/config/BeansConfig.java
@@ -1,15 +1,9 @@
 package pl.kurs.magdalena_pikulska_test_finalny.config;
 
-import jakarta.persistence.EntityManagerFactory;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
 public class BeansConfig {
@@ -19,20 +13,5 @@ public class BeansConfig {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
     }
-
-//    @Bean
-//    public TaskExecutor taskExecutor() {
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        executor.setCorePoolSize(10);
-//        executor.setMaxPoolSize(20);
-//        executor.setQueueCapacity(50);
-//        executor.setThreadNamePrefix("ImportTaskExecutor-");
-//        executor.initialize();
-//        return executor;
-//    }
-
-
-
-
 
 }
